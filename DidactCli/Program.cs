393f4ff9913084253046f67495a2cf53db50f858
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-02-10 03:08:08 -0600

    wip

--- DidactCli/Program.cs
+++ DidactCli/Program.cs
@@ -1,2 +1,105 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using DidactCli.Commands;
+using DidactCli.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+AnsiConsole.Write(
+    new FigletText("Didact Cli")
+        .LeftJustified()
+        .Color(Color.Orange1));
+
+#region Setup and bind the appsettings.
+
+var iConfiguration = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory) // Ensure correct file path
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .Build();
+var appSettings = new AppSettings();
+iConfiguration.Bind(appSettings);
+
+#endregion
+
+#region Setup dependency injection and other bootstrapping utilities.
+
+var services = new ServiceCollection();
+services.AddSingleton<IConfiguration>(iConfiguration);
+services.AddSingleton(appSettings);
+services.AddLogging(configure =>
+{
+    configure.AddConsole();
+});
+var registrar = new TypeRegistrar(services);
+var app = new CommandApp(registrar);
+
+#endregion
+
+app.Configure(config =>
+{
+    config.SetApplicationName("Didact Cli");
+    config.AddCommand<VersionCommand>("version")
+        .WithDescription("The version of Didact Cli.");
+
+    config.AddBranch("engine", engine =>
+    {
+        engine.AddCommand<EngineInstallCommand>("install")
+            .WithDescription("Installs Didact Engine to the target location.");
+        engine.AddCommand<TestCommand>("set")
+            .WithDescription("Sets the target Didact Engine.");
+        engine.AddCommand<TestCommand>("get")
+            .WithDescription("Gets the target Didact Engine.");
+        engine.AddBranch("config", config =>
+        {
+            config.AddCommand<TestCommand>("generate") // --open true option for autoopening config file.
+                .WithDescription("Generates a new runtime environment variables file for Didact Engine.");
+            config.AddCommand<TestCommand>("get")
+                .WithDescription("Gets the runtime environment variables file for Didact Engine.");
+        });
+    });
+
+    config.AddBranch("ui", ui =>
+    {
+        ui.AddCommand<UiInstallCommand>("install")
+            .WithDescription("Installs Didact UI to the target location.");
+        ui.AddCommand<TestCommand>("set")
+            .WithDescription("Sets the target Didact UI.");
+        ui.AddCommand<TestCommand>("get")
+            .WithDescription("Gets the target Didact UI.");
+        ui.AddBranch("config", config =>
+        {
+            config.AddCommand<TestCommand>("generate") // --open true option for autoopening config file.
+                .WithDescription("Generates a new runtime environment variables file for Didact UI.");
+            config.AddCommand<TestCommand>("get")
+                .WithDescription("Gets the runtime environment variables file for Didact UI.");
+        });
+    });
+
+    config.AddBranch("license", license =>
+    {
+        license.AddCommand<TestCommand>("set")
+            .WithDescription("Sets the Didact license key.");
+        license.AddCommand<TestCommand>("get")
+            .WithDescription("Gets the Didact license key.");
+        license.AddCommand<TestCommand>("authenticate")
+            .WithDescription("Authenticates the Didact license key.");
+        license.AddCommand<TestCommand>("validate")
+            .WithDescription("Validates the Didact license key.");
+    });
+
+    config.AddBranch("library", library =>
+    {
+        library.AddCommand<TestCommand>("list")
+            .WithDescription("Lists the registered Didact flow libraries.");
+        library.AddCommand<TestCommand>("add")
+            .WithDescription("Adds a Didact flow library to the registry.");
+        library.AddCommand<TestCommand>("remove")
+            .WithDescription("Removes a Didact flow library to the registry.");
+        library.AddCommand<TestCommand>("deploy")
+            .WithDescription("Deploys all flow libraries in the registry.");
+    });
+});
+
+return app.Run(args);
--- DidactCli/Services/AppSettings.cs
+++ DidactCli/Services/AppSettings.cs
@@ -2,20 +2,20 @@
 {
     public class AppSettings
     {
-        public required string TestSetting { get; set; }
+        public string TestSetting { get; set; }
 
-        public required DidactEngine DidactEngine { get; set; }
+        public DidactEngine DidactEngine { get; set; }
 
-        public required DidactUi DidactUi { get; set; }
+        public DidactUi DidactUi { get; set; }
     }
 
     public class DidactEngine
     {
-        public required string RuntimeEnvironmentVariablesFileName { get; set; }
+        public string RuntimeEnvironmentVariablesFileName { get; set; }
     }
 
     public class DidactUi
     {
-        public required string RuntimeEnvironmentVariablesFileName { get; set; }
+        public string RuntimeEnvironmentVariablesFileName { get; set; }
     }
 }
