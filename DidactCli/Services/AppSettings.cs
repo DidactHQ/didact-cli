fc647a94c13ec1d9d0834da776c898c7bb3e36bf
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-02-10 02:47:32 -0600

    Updated appsettings files

--- DidactCli/Services/AppSettings.cs
+++ DidactCli/Services/AppSettings.cs
@@ -2,6 +2,20 @@
 {
     public class AppSettings
     {
-        public string TestSetting { get; set; }
+        public required string TestSetting { get; set; }
+
+        public required DidactEngine DidactEngine { get; set; }
+
+        public required DidactUi DidactUi { get; set; }
+    }
+
+    public class DidactEngine
+    {
+        public required string RuntimeEnvironmentVariablesFileName { get; set; }
+    }
+
+    public class DidactUi
+    {
+        public required string RuntimeEnvironmentVariablesFileName { get; set; }
     }
 }
