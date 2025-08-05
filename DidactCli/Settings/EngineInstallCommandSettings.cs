adfa97b383f185c64299ee5447c4a0ff7f83885c
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-08-05 18:12:38 -0500

    wip

--- DidactCli/Settings/EngineInstallCommandSettings.cs
+++ DidactCli/Settings/EngineInstallCommandSettings.cs
@@ -5,6 +5,7 @@ namespace DidactCli.Settings
     public class EngineInstallCommandSettings : CommandSettings
     {
         [CommandOption("-p|--path <PATH>")]
+        // Add default path?
         public string InstallationPath { get; set; } = null!;
     }
 }
